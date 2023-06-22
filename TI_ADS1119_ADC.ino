be894247665bcd1c47b762186574c1e258fcc8bc
Adam V Steele <adam@avsteele.com>
2023-06-22 11:44:07 -0400

    config in examples moved to funciton

--- TI_ADS1119_ADC.ino
+++ TI_ADS1119_ADC.ino
@@ -11,30 +11,37 @@ void setup_ads1119()
 {
     Serial.println("ADS1119: Starting...");
 
-    struct Configuration config;    
-    //config.mux = Mux::positiveAIN0negativeAIN1;
-    config.mux = Mux::positiveAIN0negativeAGND;
-    
-    // select gain
-    config.gain = Gain::one;
-    //config.gain = Gain::four;
-
-    config.dataRate  = DataRate::sps20;
-    //config.dataRate  = DataRate::sps90;
-
-    config.conversionMode = ConversionMode::singleShot;
-    
-    config.voltageReference = VoltageReferenceSource::internal;
-    //config.voltageReference = VoltageReferenceSource::external;
-    //config.externalReferenceVoltage = 5.0f;
-
     bool OK = false;
     ads1.begin();
     Serial.println("ADS1119: Begin");
     OK = ads1.reset();    
     Serial << "ADS1119: Reset " << (OK ? "OK" : "ERROR") << endl;
-    OK = ads1.writeConfig(config);
+    //OK = ads1.writeConfig(config);
+    setConfig(0);
     Serial << "ADS1119: Configured " << (OK ? "OK" : "ERROR") << endl;
+}
+
+void setConfig(int whichChannel)
+{
+    struct Configuration config;    
+    //config.mux = Mux::positiveAIN0negativeAIN1;
+    if (whichChannel == 0)
+        config.mux = Mux::positiveAIN0negativeAGND;
+    else
+        config.mux = Mux::positiveAIN1negativeGND;
+
+    // select gain
+    config.gain = Gain::one;
+    //config.gain = Gain::four;
+    config.dataRate  = DataRate::sps20;
+    //config.dataRate  = DataRate::sps90;
+    config.conversionMode = ConversionMode::singleShot;    
+    
+    //config.voltageReference = VoltageReferenceSource::internal;
+    config.voltageReference = VoltageReferenceSource::external;
+    config.externalReferenceVoltage = 5.0f;
+
+    ads1.writeConfig(config);
 }
 
 void setup()
@@ -57,9 +64,18 @@ void loop()
 
 void read_and_printVolts()
 {
-    uint16_t adc = ads1.readSingleADC();
-    float volts = ads1.toVoltage(adc);
-    Serial << "adc : 0x" << _HEX(adc) << "\tV : " << _FLOAT(volts,4) << endl;
+    setConfig(0);
+    uint16_t adc0 = ads1.readSingleADC();
+    float volts0 = ads1.toVoltage(adc0);
+
+    setConfig(1);
+    uint16_t adc1 = ads1.readSingleADC();
+    float volts1 = ads1.toVoltage(adc1);
+
+
+    //Serial << "adc : 0x" << _HEX(adc0) << "\tV : " << _FLOAT(volts0,4) << endl;
+    Serial << _FLOAT(volts0,4) << "," << _FLOAT(volts1,4) << endl;
+    
     // bool ready = ads1.readStatus().dataReady == DataReady::ready;
     // Serial << "Ready :" << ready << endl;
 }
